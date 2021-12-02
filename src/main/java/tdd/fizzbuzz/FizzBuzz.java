7dbd97f640d2deba18005eb64a55086c314c3c54
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-02 14:00:56 +0800

    should_return_order_when_count_off_given_order_number_normal_case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,8 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public String countoff(int order) {
+        return String.valueOf(order);
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_order_when_count_off_given_order_number_normal_case(){
+        //given
+        int order =1;
+
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result =  fizzBuzz.countoff(order);
+
+        //then
+        assertEquals("1", result);
+    }
+
+
 }
